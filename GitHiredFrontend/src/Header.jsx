9b99548c3db96079728ac84ea11dd9322a9b5183
notduyly <162235323+notduyly@users.noreply.github.com>
2025-02-04 22:16:08 -0800

    Add Footer

--- GitHiredFrontend/src/Header.jsx
+++ GitHiredFrontend/src/Header.jsx
@@ -3,6 +3,13 @@ function Header() {
     return (
         <header>
             <h1>GitHired</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+            </nav>
+            <hr/>
         </header>
     );
 }
