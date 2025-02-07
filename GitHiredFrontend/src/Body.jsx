4e475554dc21df3b894fd434d3a2ee9e1e11b442
notduyly <162235323+notduyly@users.noreply.github.com>
2025-02-07 10:19:14 -0800

    Add logo to header

--- GitHiredFrontend/src/Body.jsx
+++ GitHiredFrontend/src/Body.jsx
@@ -1,10 +1,10 @@
-import { Container, Button } from 'react-bootstrap';
-import './styles.css';
+import { Container, Button } from "react-bootstrap";
+import "./styles.css";
 
 function Body() {
     return (
         <Container fluid className="body-container">
-            <img src='/GitHired-Logo-Transparent.png' alt='GitHired Logo' className='logo'/>
+            <img src="/GitHired-Logo-Transparent.png" alt="GitHired Logo" className="logo"/>
             <h1>Welcome to GitHired</h1>
             <Button variant="primary">Join Now</Button> 
         </Container>
--- GitHiredFrontend/src/Header.jsx
+++ GitHiredFrontend/src/Header.jsx
@@ -1,10 +1,16 @@
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Container, Navbar, Nav } from "react-bootstrap";
 
 function Header() {
     return (
         <Navbar bg="dark" variant="dark" expand="lg" className="header-container">
             <Container>
-                <Navbar.Brand href="/">GitHired</Navbar.Brand>
+                <Navbar.Brand href="/" className="clickable-logo">
+                    <img
+                        alt="GitHired Logo Word"
+                        src="/GitHired-Logo-Word.png"
+                        className="header-logo" 
+                    />
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
