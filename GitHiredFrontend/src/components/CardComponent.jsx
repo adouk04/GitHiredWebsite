2f53e6b67e54239d198800f5d39052cbd1dfcc87
Yusuf Shakhpaz <shakhpazyusuf@gmail.com>
2025-02-14 22:13:50 -0800

    added components and put more studd to about page

---
+++ GitHiredFrontend/src/components/CardComponent.jsx
@@ -0,0 +1,34 @@
+import { Card } from "react-bootstrap"
+function CardComponent({ title, imgSrc, text }) {
+    return(
+        <Card className="d-flex align-items-start backgroundcolor border-0">
+            <div className="icon-square text-dark flex-shrink-0 me-2">
+                <img src={imgSrc} width={"72px"} height={"72px"} fill={"currentColor"}/>
+            </div>
+            <div>
+                <h2>{title}</h2>
+                <p>{text}</p>
+            </div>
+            <a href="#" class="btn btn-primary">
+              Primary button
+            </a>
+        </Card>
+        
+    )
+}
+
+export default CardComponent
+
+
+{/* <div class="col d-flex align-items-start">
+        <div class="icon-square bg-light text-dark flex-shrink-0 me-3">
+          <svg class="bi" width="1em" height="1em"><use xlink:href="#toggles2"></use></svg>
+        </div>
+        <div>
+          <h2>Featured title</h2>
+          <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+          <a href="#" class="btn btn-primary">
+            Primary button
+          </a>
+        </div>
+</div> */}
---
+++ GitHiredFrontend/src/components/InfoSection.jsx
@@ -0,0 +1,37 @@
+import { Container, Col, Row, Button } from "react-bootstrap"
+
+
+function InfoSection({ title, text, imgSrc, buttonText, imageRight }) {
+    console.log("InfoSection component is rendering");
+    return (
+        <Container className='py-4'>
+            <Row className='d-flex pb-4 align-items-center'>
+                {imageRight ? (
+                <>
+                    <Col md={7} className='text-start'>
+                        <h1>{title}</h1>
+                        <p>{text}</p>
+                        <Button>{buttonText}</Button>
+                    </Col>
+                    <Col md={5} className='d-flex'>
+                        <img className="d-block logo m-auto" src={imgSrc} alt="GitHired Logo"/>
+                    </Col>
+                </>
+                ) : (
+                <>
+                    <Col md={5} className='d-flex'>
+					<img className="d-block logo m-auto" src={imgSrc} alt="GitHired Logo"/>
+                    </Col>
+                    <Col md={7} className='text-end'>
+                        <h1>{title}</h1>
+                        <p>{text}</p>
+                        <Button>{buttonText}</Button>
+                    </Col>
+                </>
+                )}
+            </Row>
+        </Container>
+    )
+}
+
+export default InfoSection
