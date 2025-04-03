6b87f1f67fe019947c4b530b271a7be6519b2596
adouk04 <alexdouk253@gmail.com>
2025-04-03 12:50:10 -0700

    added title to events tab

--- GitHiredFrontend/src/Events.jsx
+++ GitHiredFrontend/src/Events.jsx
@@ -1,8 +1,14 @@
 import EventTable from './components/Table';
+import { Container } from 'react-bootstrap';
 
 function Events() {
     return (
         <div className = "backgroundcolor">
+            <Container fluid className='px-4 py-4 text-center'>
+                <h1 class="display-5 fw-bold text-body-emphasis">
+                    Meeting Times and Events
+                </h1>
+            </Container>
             <EventTable/>
         </div>
     );
